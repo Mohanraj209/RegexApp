a574c27ea34e3abfe8e79f52a6c039eafa9e743d
Mohanraj209 <mohanraj1715@gmail.com>
2022-03-09 00:58:41 +0530

    Displaying Message

---
+++ Regex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("--------** User Registration **--------");
+        }
+    }
+}
