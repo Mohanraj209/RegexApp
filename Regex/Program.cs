75a4d0cbaf7f725577b94a38846816bfeada1075
Mohanraj209 <mohanraj1715@gmail.com>
2022-03-12 14:01:24 +0530

    Program to check the entered First Name is valid or not

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Regex
+namespace RegexApp
 {
     internal class Program
     {
         public static void Main()
         {
             Console.WriteLine("--------** User Registration **--------");
+            User_Registration user  = new User_Registration();  
+            if (user.validateFirstName("Abin"))
+            {
+                Console.WriteLine("\n Valid");
+            }
+            else
+                Console.WriteLine("\n Invalid");
+            Console.ReadKey();
         }
     }
 }
---
+++ Regex/User_Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexApp
+{
+	internal class User_Registration
+	{
+		public static string Pattern = "^([A-Z]*[a-z]*){2,}$";
+		public bool validateFirstName(string sample)
+		{
+			return Regex.IsMatch(sample, Pattern);
+		}
+	}
+}
